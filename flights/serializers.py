42a411e9e6648118e5a417c9578015c131a96c90
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-07-24 20:57:59 +0300

    Thank you Laila for your patience with our spelling errors

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import Flight, Booking
+from django.contrib.auth.models import User
 
 
 class FlightSerializer(serializers.ModelSerializer):
@@ -25,4 +26,24 @@ class UpdateBookingSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Booking
 		fields = ['date', 'passengers']
+
+class RegisterSerializer(serializers.ModelSerializer):
+	password = serializers.CharField(write_only=True)
+	class Meta:
+		model = User
+		fields = ['username', 'password', 'last_name', 'first_name']
+
+	def create(self, validated_data):
+		u = validated_data["username"]
+		p = validated_data["password"]
+		f = validated_data["first_name"]
+		l = validated_data["last_name"]
+
+		user = User(username = u, first_name = f, last_name = l)
+		user.set_password(p)
+		user.save()
+
+		return validated_data
+
+
 
